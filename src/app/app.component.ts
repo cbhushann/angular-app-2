27607916bf14886829c0d2fc5936780ba93cfee8
cbhushann <45370421+cbhushann@users.noreply.github.com>
2021-06-25 22:01:22 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello Angular App World!!!';
+  title = 'Hello Angular Application World!!!';
 }
